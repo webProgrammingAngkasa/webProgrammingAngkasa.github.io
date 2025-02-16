5e910cee6aa59f88d2fc3248d4459fe6b1fe8a5c
Zakaria-cpu301 <ramadanzaka27@gmail.com>
2025-02-16 07:52:59 +0700

    proses penyimpanan data pengunjung ke database

---
+++ sewa_hotel/proses_simpan_pengunjung.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+include 'main.php';
+include '../connect/conn.php';
+$hotel = new Hotel('localhost', 'root', '', 'sewa_hotel');
+
+$dataPengunjung = [
+    //! pengunjung
+    'nama' => $_POST['nama'],
+    'alamat' => $_POST['alamat'],
+    'no_tlp' => $_POST['no_tlp']
+];
+
+$dataPemesanan = [
+    //! pemesanan
+    'no_kamar' => $_POST['no_kamar'],
+    'check_in' => $_POST['check_in'],
+    'check_out' => $_POST['check_out'],
+    'note' => $_POST['note']
+];
+
+if (
+    !empty($dataPengunjung['nama']) &&
+    !empty($dataPengunjung['alamat']) &&
+    !empty($dataPengunjung['no_tlp']) &&
+    !empty($dataPemesanan['no_kamar']) &&
+    !empty($dataPemesanan['check_in']) &&
+    !empty($dataPemesanan['check_out'])
+) {
+    if ($hotel->dataPemesanan($dataPemesanan) && ($hotel->dataPengunjung($dataPengunjung)) && ($hotel->updateNoRoom($dataPemesanan))) {
+        echo '
+            <script> 
+                alert("data berhasil ditambahkan..."); 
+                document.location.href = "payment_result.php";
+                </script>
+                ';
+    }
+} else {
+    echo '
+    <script> 
+        alert("Mohon isi semua data dengan lengkap."); 
+        document.location.href = "form_pesanan.php";
+    </script>
+';
+}
