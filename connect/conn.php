ec58046d04a28da1d80b020b37dd4de7cf140d24
Zakaria-cpu301 <ramadanzaka27@gmail.com>
2025-02-16 07:12:49 +0700

    koneksi ke database sewa_hotel

---
+++ connect/conn.php
@@ -0,0 +1,9 @@
+<!-- pemberi harapan palsu -->
+ <?php
+ $hostname = 'localhost';
+ $username  = 'root';
+ $password = '';
+ $database = 'sewa_hotel';
+
+ $dbConnect = mysqli_connect($hostname, $username, $password, $database);
+ ?>
